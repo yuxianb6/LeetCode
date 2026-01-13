62db669f7c323c4c99ff189ae1a0227b42b6c951
yuxianb6 <yuxianb6@gmail.com>
2026-01-12 20:49:26 -0500

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- array/jump-game-ii.java
+++ array/jump-game-ii.java
@@ -1,13 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
         int pos=0;
         int curMax=nums[pos];
-        int ans=1;
+        int max=curMax;
+        int ans=0;
         while(pos<nums.length){
             if(pos==curMax){
                 ans++;
+                curMax=max;
             }
-            curMax=Math.max(pos+nums[pos],curMax);
+            max=Math.max(pos+nums[pos],max);
             pos++;
         }
         return ans;
