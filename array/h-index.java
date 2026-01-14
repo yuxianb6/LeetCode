cb07c5113572f7414ec01a1b68b2bce2ec26a7af
yuxianb6 <yuxianb6@gmail.com>
2026-01-13 19:35:02 -0500

    LC-h-index [Runtime: N/A; Memory: N/A]

--- array/h-index.java
+++ array/h-index.java
@@ -3,8 +3,8 @@ class Solution {
         Arrays.sort(citations);
         int ans=0;
         int n=citations.length;
-        for(int i=citations.length-1;i>0;i--){
-            if (citations[i]<=n-i+1) {
+        for(int i=citations.length-1;i>=0;i--){
+            if (citations[i]<=n-i) {
                 ans=citations[i];
                 break;
             }
