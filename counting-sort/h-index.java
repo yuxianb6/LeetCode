e9d917420cefa4e800e8659918ff4d92c92d1083
yuxianb6 <yuxianb6@gmail.com>
2026-01-13 19:41:34 -0500

    LC-h-index [Runtime: 5 ms; Memory: 43.9 MB]

--- counting-sort/h-index.java
+++ counting-sort/h-index.java
@@ -1,18 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int ans=0;
+
         int n=citations.length;
-        for(int i=citations.length-1;i>=0;i--){
-            if (citations[i]<=n-i) {
-                ans=citations[i];
-                break;
+        for(int i=citations.length;i>0;i--){
+            if (citations[n-i]>=i) {
+                return i;
+
             }
         }
-        if(ans==0){
-            return 1;
-        }
-        return ans;
+        return 0;
         
     }
 }
