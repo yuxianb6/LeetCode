f172d3b17c3ada5b84354889d2534eca03735060
yuxianb6 <yuxianb6@gmail.com>
2026-01-13 19:35:38 -0500

    LC-h-index [Runtime: N/A; Memory: N/A]

--- sorting/h-index.java
+++ sorting/h-index.java
@@ -1,7 +1,7 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int ans=0;
+        int ans=1;
         int n=citations.length;
         for(int i=citations.length-1;i>=0;i--){
             if (citations[i]<=n-i) {
