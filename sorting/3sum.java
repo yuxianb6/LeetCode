04861eb50ecfe67cc36d232808604ca4ab4a3a9e
yuxianb6 <yuxianb6@gmail.com>
2026-01-19 00:53:04 -0500

    LC-3sum [Runtime: 35 ms; Memory: 59.2 MB]

--- sorting/3sum.java
+++ sorting/3sum.java
@@ -12,6 +12,8 @@ class Solution {
                 while(left<right){
                     if(nums[left]+nums[right]==-nums[i]){
                         ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right-1]) right--;
                         left++;
                         right--;
                     }else if(nums[left]+nums[right]>-nums[i]){
