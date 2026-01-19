aa920ceac8b2f39c1b4f529861c4710da0ce39bf
yuxianb6 <yuxianb6@gmail.com>
2026-01-19 00:50:22 -0500

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.java
+++ sorting/3sum.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        int n=nums.length;
+        List<List<Integer>>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i!=0&&nums[i]==nums[i-1]){
+                continue;
+            }else{
+                int left=i+1;
+                int right=nums.length-1;
+                while(left<right){
+                    if(nums[left]+nums[right]==-nums[i]){
+                        ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        left++;
+                        right--;
+                    }else if(nums[left]+nums[right]>-nums[i]){
+                        right--;
+                    }else{
+                        left++;
+                    }
                 }
             }
+
         }
         return ans;
     }
