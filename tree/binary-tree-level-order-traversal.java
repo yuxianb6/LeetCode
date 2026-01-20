7018392aab25595988b7c3984a71376a30d7399b
yuxianb6 <yuxianb6@gmail.com>
2026-01-20 11:45:30 -0500

    LC-binary-tree-level-order-traversal [Runtime: 2 ms; Memory: 46.7 MB]

--- tree/binary-tree-level-order-traversal.java
+++ tree/binary-tree-level-order-traversal.java
@@ -1,39 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root==null){
-            return ans;
-        }
-        Queue<TreeNode>queue=new LinkedList<>();
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
-        while(!queue.isEmpty()){
-            List<Integer>level=new ArrayList<>();
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                level.add(node.val);
-                if(node.left!=null)queue.add(node.left);
-                if(node.right!=null)queue.add(node.right);
-               
+        int level = 0;
+        while (!queue.isEmpty()) {
+            // start the current level
+            levels.add(new ArrayList<Integer>());
+
+            // number of elements in the current level
+            int level_length = queue.size();
+            for (int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                // fulfill the current level
+                levels.get(level).add(node.val);
+
+                // add child nodes of the current level
+                // in the queue for the next level
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
-            ans.add(level);
+            // go to next level
+            level++;
         }
-        return ans;
-      
+        return levels;
     }
 }
