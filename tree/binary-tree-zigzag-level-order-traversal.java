9ab7ecd1b06427e2165cbfa2d2270254482de7aa
yuxianb6 <yuxianb6@gmail.com>
2026-01-20 12:05:31 -0500

    LC-binary-tree-zigzag-level-order-traversal [Runtime: 1 ms; Memory: 43.9 MB]

--- tree/binary-tree-zigzag-level-order-traversal.java
+++ tree/binary-tree-zigzag-level-order-traversal.java
@@ -1,46 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>>ans=new ArrayList<>();
-        if(root==null){
-            return ans;
-        }
-        Queue<TreeNode>queue=new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        int count=0;
-        while(!queue.isEmpty()){
-            List<Integer>level=new ArrayList<>();
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();
-                level.add(node.val);
-                if(node.left!=null){
-                    queue.add(node.left);
-                }
-                if(node.right!=null){
-                    queue.add(node.right);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Deque<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
                 }
 
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
-            if(count%2==1){
-                Collections.reverse(level);
-            }
-            count++;
-            ans.add(level);
+
+            ans.add(new ArrayList<>(level));
+            leftToRight = !leftToRight;
         }
         return ans;
     }
