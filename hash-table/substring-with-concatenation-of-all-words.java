bf119d5f1580e6834b6ef60fc0b41f7368073094
yuxianb6 <yuxianb6@gmail.com>
2026-01-19 19:07:00 -0500

    LC-substring-with-concatenation-of-all-words [Runtime: 12 ms; Memory: 47.1 MB]

--- hash-table/substring-with-concatenation-of-all-words.java
+++ hash-table/substring-with-concatenation-of-all-words.java
@@ -1,43 +1,43 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        int window = words.length * words[0].length();
-        int wordLen = words[0].length();
+        int wordLen=words[0].length();
         List<Integer> ans = new ArrayList<>();
-        
-        for(int i = 0; i <= s.length() - window; i++) {  // 修正1
-            if(isConcatenation(s.substring(i, i + window), words)) {  // 修正2
-                ans.add(i);
+        int wordCount = words.length;
+        HashMap<String,Integer>targetCount=new HashMap<>();
+        for(int i=0;i<words.length;i++){
+            if(targetCount.containsKey(words[i])){
+                targetCount.put(words[i], targetCount.get(words[i])+1);
+            }else{
+                targetCount.put(words[i], 1);
             }
         }
-        return ans;  
-    }
-    
-    public boolean isConcatenation(String word, String[] words) {
-        HashMap<String, Integer> counts = new HashMap<>();
-        
-        for(String w : words) {
-            counts.put(w, counts.getOrDefault(w, 0) + 1);
-        }
-        
-        for(int i = 0; i <= word.length() - words[0].length(); i += words[0].length()) {  
-            String piece = word.substring(i, i + words[0].length());
-            
-            if(!counts.containsKey(piece)) {
-                return false;
+
+        for(int offset=0;offset<words[0].length();offset++){
+            HashMap<String, Integer> windowCount = new HashMap<>();
+            int left = offset;
+            int count = 0;
+            for (int right = offset; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+                if (!targetCount.containsKey(word)) {
+                    windowCount.clear();
+                    count = 0;
+                    left = right + wordLen;
+                    continue;
+                }
+                windowCount.put(word, windowCount.getOrDefault(word, 0) + 1);
+                count++;
+                while (windowCount.get(word) > targetCount.get(word)) {
+                    String leftWord = s.substring(left, left + wordLen);
+                    windowCount.put(leftWord, windowCount.get(leftWord) - 1);
+                    left += wordLen;
+                    count--;
+                }
+                if (count == wordCount) {
+                    ans.add(left);
+                }
             }
-            
-            counts.put(piece, counts.get(piece) - 1);
-            if(counts.get(piece) < 0) { 
-                return false;
-            }
         }
-        
-        for(int count : counts.values()) {
-            if(count != 0) {
-                return false;
-            }
-        }
-        
-        return true;
+        return ans;
     }
 }
