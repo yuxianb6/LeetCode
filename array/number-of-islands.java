5a3e03b79d66f3d70af2793c71613a03fda85cca
yuxianb6 <yuxianb6@gmail.com>
2026-01-28 21:47:42 -0500

    LC-number-of-islands [Runtime: 4 ms; Memory: 52.7 MB]

--- array/number-of-islands.java
+++ array/number-of-islands.java
@@ -6,35 +6,39 @@ class Solution {
         boolean[][]visited=new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if((!visited[i][j])&&grid[i][j]=='1'){
-                    dfs(i,j,visited,grid);
+                if(!visited[i][j]&&grid[i][j]=='1'){
+                    dfs(i,j,grid,visited);
                     ans++;
                 }
             }
         }
         return ans;
+
     }
-    public void dfs(int row,int col,boolean[][]visited,char[][]grid){
+    public void dfs(int row,int col,char[][]grid,boolean[][]visited){
         int m=grid.length;
         int n=grid[0].length;
-        
-
-       
-        if(row<0||col<0||row>=m||col>=n||grid[row][col]=='0'){
+        if(row<0||col<0||col>=n||row>=m){
             return;
-        }else if(visited[row][col]){
-            return;
-
-            
-
-        }else{
-            visited[row][col]=true;
-            dfs(row+1,col,visited,grid);
-            dfs(row-1,col,visited,grid);
-            dfs(row,col+1,visited,grid);
-            dfs(row,col-1,visited,grid);
-            
         }
+        if(grid[row][col]=='0'){
+            return;
+        }
+        if(visited[row][col]){
+            return;
+        }
+        visited[row][col]=true;
+        dfs(row+1,col,grid,visited);
+        dfs(row-1,col,grid,visited);
+        dfs(row,col+1,grid,visited);
+        dfs(row,col-1,grid,visited);
+        return;
+
+
+
+
     }
+
+
 
 }
