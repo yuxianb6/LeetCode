d7199f14e3d9d6a813875bf05cac72c129c47192
yuxianb6 <yuxianb6@gmail.com>
2026-02-01 16:49:21 -0500

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/linked-list-cycle-ii.java
+++ two-pointers/linked-list-cycle-ii.java
@@ -1,69 +1,34 @@
-// /**
-//  * Definition for singly-linked list.
-//  * class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode(int x) {
-//  *         val = x;
-//  *         next = null;
-//  *     }
-//  * }
-//  */
-// public class Solution {
-//     public ListNode detectCycle(ListNode head) {
-//         ListNode slow=head;
-//         ListNode fast=head;
-//         while(fast!=null&&fast.next!=null){
-//             slow=slow.next;
-//             fast=fast.next.next;
-//             if(slow==fast){
-//                 break;
-//             }
-        
-//         }
-//         if(slow==null||fast==null){
-//             return null;
-//         }
-//         fast=head;
-//         while(fast!=slow){
-//             slow=slow.next;
-//             fast=fast.next;
-//         }
-//         return fast;
-//     }
-// }
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // Initialize tortoise and hare pointers
-        ListNode tortoise = head;
-        ListNode hare = head;
-
-        // Move tortoise one step and hare two steps
-        while (hare != null && hare.next != null) {
-            tortoise = tortoise.next;
-            hare = hare.next.next;
-
-            // Check if the hare meets the tortoise
-            if (tortoise == hare) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 break;
             }
+        
         }
-
-        // Check if there is no cycle
-        if (hare == null || hare.next == null) {
+        if(fast.next==null||fast==null){
             return null;
         }
-
-        // Reset either tortoise or hare pointer to the head
-        hare = head;
-
-        // Move both pointers one step until they meet again
-        while (tortoise != hare) {
-            tortoise = tortoise.next;
-            hare = hare.next;
+        fast=head;
+        while(fast!=slow){
+            slow=slow.next;
+            fast=fast.next;
         }
-
-        // Return the node where the cycle begins
-        return tortoise;
+        return fast;
     }
 }
