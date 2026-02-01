97f8c732f45f480ec6b284a370b951c032ac6016
yuxianb6 <yuxianb6@gmail.com>
2026-02-01 17:05:35 -0500

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/non-overlapping-intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                } else {
+                    return a[0] - b[0];
+                }
+            }
+        });
+        int ans=0;
+        for(int i=0;i<intervals.length-1;i++){
+            if(intervals[i][1]>intervals[i+1][0]){
+                ans++;
+                intervals[i+1][0]=intervals[i][0];
+                intervals[i+1][1]=intervals[i][1];
+            }
+        }
+        return ans;
+    }
+}
