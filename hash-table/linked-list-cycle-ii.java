a4209dd6e52b1cad18d64325fe4d82e5625a2740
yuxianb6 <yuxianb6@gmail.com>
2026-02-01 16:47:13 -0500

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- hash-table/linked-list-cycle-ii.java
+++ hash-table/linked-list-cycle-ii.java
@@ -13,7 +13,7 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
-        while(fast.next!=null){
+        while(fast!=null&&fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
             if(slow==fast){
