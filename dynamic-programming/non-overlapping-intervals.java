60ac8f328c7746a27a6164307ab2513700f491fb
yuxianb6 <yuxianb6@gmail.com>
2026-02-01 17:14:48 -0500

    LC-non-overlapping-intervals [Runtime: 61 ms; Memory: 116.2 MB]

--- dynamic-programming/non-overlapping-intervals.java
+++ dynamic-programming/non-overlapping-intervals.java
@@ -14,8 +14,10 @@ class Solution {
         for(int i=0;i<intervals.length-1;i++){
             if(intervals[i][1]>intervals[i+1][0]){
                 ans++;
-                intervals[i+1][0]=intervals[i][0];
-                intervals[i+1][1]=intervals[i][1];
+                if(intervals[i][1]<intervals[i+1][1]){
+                    intervals[i+1][0]=intervals[i][0];
+                    intervals[i+1][1]=intervals[i][1];
+                }
             }
         }
         return ans;
