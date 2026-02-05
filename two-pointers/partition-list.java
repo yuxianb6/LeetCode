1c47ccfc49af52c54a462bfb5279f4b822859896
yuxianb6 <yuxianb6@gmail.com>
2026-02-05 01:22:21 -0500

    LC-partition-list [Runtime: 0 ms; Memory: 43.8 MB]

---
+++ two-pointers/partition-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallDummy = new ListNode(0);
+        ListNode largeDummy = new ListNode(0);
+
+        ListNode small = smallDummy;
+        ListNode large = largeDummy;
+
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val < x) {
+                small.next = cur;
+                small = small.next;
+            } else {
+                large.next = cur;
+                large = large.next;
+            }
+            cur = cur.next;
+        }
+
+        large.next = null;              
+        small.next = largeDummy.next;   
+
+        return smallDummy.next;
+    }
+}
