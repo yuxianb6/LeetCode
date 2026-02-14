da54de916a9c9a2daee2a70dcc0248ba3996b5cf
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 18:48:23 -0500

    LC-monthly-transactions-i [Runtime: 537 ms; Memory: 0B]

--- database/monthly-transactions-i.sql
+++ database/monthly-transactions-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select Date_Format((trans_date),'%Y-%m')as month,country,count(id)as trans_count,sum(case when state='approved' then 1 else 0 end)as approved_count,sum(amount)as trans_total_amount,sum(case when state='approved' then amount else 0 end)as approved_total_amount
 from transactions
-group by month(trans_date),country
+group by month,country
