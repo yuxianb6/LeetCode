e14d4a0c0929ee811bf6a2ef829a8110a363f6ee
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 01:02:01 -0500

    LC-customer-who-visited-but-did-not-make-any-transactions [Runtime: 1526 ms; Memory: 0B]

--- database/customer-who-visited-but-did-not-make-any-transactions.sql
+++ database/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select customer_id,sum(case when amount is null then 1 else 0 end)as count_no_trans
+select customer_id,sum(case when transaction_id is null then 1 else 0 end)as count_no_trans
 from visits v
 left join transactions t
 on v.visit_id=t.visit_id
 group by customer_id
-having sum(case when amount is null then 1 else 0 end)>0
+having sum(case when transaction_id is null then 1 else 0 end)>0
