43a7191aadb72abeef70fe07de5fff887c481740
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 17:17:33 -0500

    LC-students-and-examinations [Runtime: 937 ms; Memory: 0B]

--- database/students-and-examinations.sql
+++ database/students-and-examinations.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-select s.student_id,s.student_name,sub.subject_name,coalesce(count(e.subject_name),0) as attended_exams
-from students s cross join subjects sub left join examinations e
-on sub.subject_name=e.subject_name and s.student_id=e.student_id
-group by s.student_id,sub.subject_name
-order by s.student_id,s.student_name,sub.subject_name
+select stu.student_id,stu.student_name,sub.subject_name,sum(case when e.student_id is not null then 1 else 0 end)as attended_exams
+from students stu
+cross join subjects sub
+left join examinations e
+on stu.student_id=e.student_id
+and sub.subject_name=e.subject_name
+group by stu.student_id,sub.subject_name
+order by stu.student_id,sub.subject_name
