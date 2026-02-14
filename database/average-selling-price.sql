7027b74a91052878a35019f2496669acb04ee7e7
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 18:08:49 -0500

    LC-average-selling-price [Runtime: 779 ms; Memory: 0B]

--- database/average-selling-price.sql
+++ database/average-selling-price.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select p.product_id,(case when sum(u.units)=0 then 0.00 else round(sum(u.units*p.price)/sum(u.units),2) end) as average_price
+select p.product_id,(case when sum(u.units) is null then 0.00 else round(sum(u.units*p.price)/sum(u.units),2) end) as average_price
 from prices p
 left join unitssold u
 on p.product_id=u.product_id
