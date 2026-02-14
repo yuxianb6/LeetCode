a7e657b27e93c1ea9060c45110d44241e130648f
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 18:37:36 -0500

    LC-queries-quality-and-percentage [Runtime: 381 ms; Memory: 0B]

--- database/queries-quality-and-percentage.sql
+++ database/queries-quality-and-percentage.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select query_name,round(avg(rating/position),2)as quality,round(sum(case when rating<3 then 1 else 0 end)/count(*)*100,2) as poor_query_percentage
-from Queries
+select query_name,round(avg(rating/position),2) as quality,round(sum(case when rating<3 then 1 else 0 end)*100/count(query_name),2)as poor_query_percentage
+from queries
 group by query_name
+
