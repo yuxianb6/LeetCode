a38c310fb46fd9b87b7a8aa1cffc8dd1a25672a2
yuxianb6 <yuxianb6@gmail.com>
2026-02-14 18:20:52 -0500

    LC-project-employees-i [Runtime: 479 ms; Memory: 0B]

--- database/project-employees-i.sql
+++ database/project-employees-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select project_id,avg(experience_years)as average_years
+select project_id,round(avg(experience_years),2)as average_years
 from project p
 join employee e
 on p.employee_id=e.employee_id
