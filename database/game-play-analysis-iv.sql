60088dda06a784d80190f49e8b21972a0bb188ec
yuxianb6 <yuxianb6@gmail.com>
2026-02-16 18:21:32 -0500

    LC-game-play-analysis-iv [Runtime: 1000 ms; Memory: 0B]

--- database/game-play-analysis-iv.sql
+++ database/game-play-analysis-iv.sql
@@ -1,17 +1,15 @@
-SELECT
-  ROUND(
-    COUNT(A1.player_id)
-    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
-  , 2) AS fraction
-FROM
-  Activity A1
-WHERE
-  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
-    SELECT
-      A2.player_id,
-      MIN(A2.event_date)
-    FROM
-      Activity A2
-    GROUP BY
-      A2.player_id
-  );
+with first_log as(
+    select player_id,min(event_date) as event_date
+    from activity
+    group by player_id
+),second_day as(
+    select a.player_id 
+    from first_log f
+    left join activity a
+    on f.player_id=a.player_id
+    and a.event_date=f.event_date+interval 1 day
+)
+select round(count(s.player_id)/count(f.player_id),2) as fraction
+from first_log f
+left join second_day s
+on f.player_id=s.player_id
