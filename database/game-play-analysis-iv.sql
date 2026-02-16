cdc67faebeedbae96af731f3e0dda8f8a5e75049
yuxianb6 <yuxianb6@gmail.com>
2026-02-16 18:22:33 -0500

    LC-game-play-analysis-iv [Runtime: 492 ms; Memory: 0B]

--- database/game-play-analysis-iv.sql
+++ database/game-play-analysis-iv.sql
@@ -2,14 +2,10 @@ with first_log as(
     select player_id,min(event_date) as event_date
     from activity
     group by player_id
-),second_day as(
-    select a.player_id 
-    from first_log f
-    left join activity a
-    on f.player_id=a.player_id
-    and a.event_date=f.event_date+interval 1 day
 )
-select round(count(s.player_id)/count(f.player_id),2) as fraction
+select round(count(a.player_id)/count(f.player_id),2) as fraction
 from first_log f
-left join second_day s
-on f.player_id=s.player_id
+left join activity a
+on f.player_id=a.player_id
+and a.event_date=f.event_date+interval 1 day
+
