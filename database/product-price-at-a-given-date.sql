060f4e90e1139d34fa0b3ee7ce396fb3e866d2da
yuxianb6 <yuxianb6@gmail.com>
2026-02-17 21:23:48 -0500

    LC-product-price-at-a-given-date [Runtime: 406 ms; Memory: 0B]

--- database/product-price-at-a-given-date.sql
+++ database/product-price-at-a-given-date.sql
@@ -1,8 +1,27 @@
-select 
-product_id,
-coalesce(
-    max(case when change_date<='2019-08-16' then new_price end),
-    10
-) as price
-from products
-group by product_id
+SELECT
+  product_id,
+  10 AS price
+FROM
+  Products
+GROUP BY
+  product_id
+HAVING
+  MIN(change_date) > '2019-08-16'
+UNION ALL
+SELECT
+  product_id,
+  new_price AS price
+FROM
+  Products
+WHERE
+  (product_id, change_date) IN (
+    SELECT
+      product_id,
+      MAX(change_date)
+    FROM
+      Products
+    WHERE
+      change_date <= '2019-08-16'
+    GROUP BY
+      product_id
+  )
