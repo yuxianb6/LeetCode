b91520f0da49408a6b43eac3cc7782dc5b5d718e
yuxianb6 <yuxianb6@gmail.com>
2026-02-17 21:22:40 -0500

    LC-product-price-at-a-given-date [Runtime: N/A; Memory: N/A]

--- database/product-price-at-a-given-date.sql
+++ database/product-price-at-a-given-date.sql
@@ -1,16 +1,8 @@
-with closest as(
-    select product_id,max(change_date) as closest_date
-    from products
-    where change_date<='2019-08-16'
-    group by product_id
-),
-list as(
-    select distinct product_id
-    from products
-)
-select l.product_id,(case when c.closest_date is not null then p.new_price else 10 end)as price
-from list l left join closest c 
-on l.product_id=c.product_id
-left join products p
-on c.product_id=p.product_id
-and p.change_date=c.closest_date
+select 
+product_id,
+coalesce(
+    max(case when change_date<='2019-08-16' then new_price end),
+    10
+) as price
+from products
+group by product_id
