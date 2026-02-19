3a08ea4e6ada5e403b474f037514460ac52e050d
yuxianb6 <yuxianb6@gmail.com>
2026-02-18 19:34:39 -0500

    LC-second-highest-salary [Runtime: 262 ms; Memory: 0B]

--- database/second-highest-salary.sql
+++ database/second-highest-salary.sql
@@ -1,6 +1,6 @@
-SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+select max(t.salary) as SecondHighestSalary
+from(
+    select salary,dense_rank()over(order by salary desc) as rn
+    from Employee
+) t
+where rn=2
