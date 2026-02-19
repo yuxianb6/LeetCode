f55d3d9d4744e8e7e52088bc9b78f75494b7fb25
yuxianb6 <yuxianb6@gmail.com>
2026-02-18 19:05:33 -0500

    LC-investments-in-2016 [Runtime: N/A; Memory: N/A]

---
+++ database/investments-in-2016.sql
@@ -0,0 +1,25 @@
+with qualified1 as(
+    select i1.pid
+    from insurance i1
+    where exists(
+        select 1
+        from insurance i2
+        where i1.pid<>i2.pid
+        and i1.tiv_2015=i2.tiv_2015
+    )
+),
+qualified2 as(
+    select i1.pid
+    from insurance i1
+    where not exists(
+        select 1
+        from insurance i2
+        where i1.pid<>i2.pid
+        and i1.lon=i2.lon
+        and i1.lat=i2.lat
+    )
+)
+select sum(tiv_2016) as tiv_2016
+from insurance
+where pid in (select pid from qualified1)
+and pid in (select pid from qualified2)
