898910e8ffc4bba36fdf015e0ae913057d82a179
yuxianb6 <yuxianb6@gmail.com>
2026-02-25 17:05:01 -0500

    LC-customers-who-bought-all-products [Runtime: 550 ms; Memory: 0B]

--- database/customers-who-bought-all-products.sql
+++ database/customers-who-bought-all-products.sql
@@ -1,3 +1,13 @@
-select customer_id from customer
-group by customer_id
-having count(distinct product_key)=(select count(distinct product_key) from product)
+with cleaned as(
+    select c.customer_id,count(distinct c.product_key) as cnt
+    from product p
+    join customer c
+    on p.product_key=c.product_key
+    group by c.customer_id
+)
+select customer_id
+from cleaned
+where cnt>=(
+    select count(distinct product_key)
+    from product
+)
