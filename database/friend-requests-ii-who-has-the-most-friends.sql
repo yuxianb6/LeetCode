67d6270b615356f98d545b8f53da95d290f0ef9d
yuxianb6 <yuxianb6@gmail.com>
2026-02-25 17:37:36 -0500

    LC-friend-requests-ii-who-has-the-most-friends [Runtime: 280 ms; Memory: 0B]

--- database/friend-requests-ii-who-has-the-most-friends.sql
+++ database/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,21 +1,9 @@
-WITH ids AS (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION
-    SELECT accepter_id  AS id FROM RequestAccepted
-),
-cnts AS (
-    SELECT
-        i.id,
-        SUM(CASE WHEN r.requester_id = i.id THEN 1 ELSE 0 END) +
-        SUM(CASE WHEN r.accepter_id  = i.id THEN 1 ELSE 0 END) AS num
-    FROM ids i
-    LEFT JOIN RequestAccepted r
-      ON r.requester_id = i.id OR r.accepter_id = i.id
-    GROUP BY i.id
-)
-SELECT id, num
-FROM (
-    SELECT id, num, DENSE_RANK() OVER (ORDER BY num DESC) AS rk
-    FROM cnts
-) t
-WHERE rk = 1;
+select id, count(*) as num
+from (
+    select requester_id as id from requestAccepted
+    union all 
+    select accepter_id as id from requestAccepted
+) as all_ids
+group by id
+order by num desc
+limit 1;
